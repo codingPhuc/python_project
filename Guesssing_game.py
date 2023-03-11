8064eed2a70c6c2cb94b6b9e852e27109430ed17
codingPhuc <125779252+codingPhuc@users.noreply.github.com>
2023-03-11 15:45:07 +0700

    first commit

---
+++ Guesssing_game.py
@@ -0,0 +1,7 @@
+# implement a guessing game where a computer have a serect number an we are trying to guess that sercert number 
+# 1 make the computer generate a secert number (random.randit)
+# 
+import random 
+def guess(x) :
+    pass
+    
